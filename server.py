785162b37f0cb8aed0aa2945a5eac92b56235e55
Олег Гавришків <olehtvink@gmail.com>
2026-01-12 22:11:36 +0200

    Refactor server.py to improve structure and logging

--- server.py
+++ server.py
@@ -65,6 +65,17 @@ CORS(app, resources={r"/*": {"origins": ALLOWED_ORIGINS}},
      allow_headers=["Content-Type", "Authorization"], 
      methods=["GET", "POST", "OPTIONS"])
 
+@app.after_request
+def add_cors_headers(response):
+    origin = request.headers.get("Origin")
+    if origin in ALLOWED_ORIGINS:
+        response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    return response
+
+
 app.config["SECRET_KEY"] = SECRET
 
 # -------------------------
@@ -507,4 +518,5 @@ if __name__ == "__main__":
     logger.info("Starting API Server with Waitress on port 5001...")
     # Обов'язково вкажіть threads, оскільки Postgres блокує з'єднання
     serve(app, host="0.0.0.0", port=5001, threads=4)
+
 
